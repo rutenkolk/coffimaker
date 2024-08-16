293bb7e7d7ca994bba661f68aa6905e613e26815
Kristin Rutenkolk <kristin.rutenkolk@hhu.de>
2024-08-16 18:32:52 +0200

    first draft of header translation

--- src/clj/coffimaker/core.clj
+++ src/clj/coffimaker/core.clj
@@ -27,8 +27,41 @@
    )
   )
 
+(defn- copy-resource-to [resource target-dir]
+  (let [target-file (rfs/file target-dir resource)]
+    (io/copy (io/input-stream (io/resource resource)) target-file)
+    target-file))
 
 (comment
+
+  (println (slurp (io/resource "coffimaker.zig")))
+
+  (let [tmp (rfs/temp-dir "coffimaker")
+        _ (copy-resource-to "coffimaker.zig" tmp)
+        build-file (copy-resource-to "build.zig" tmp)
+        ]
+    (->
+     "c:/Users/Kristin/repos/zigclj/raylib.h"
+     (z/translate-c-header!
+      {:compile-error-replacements {"RL_MALLOC"  "\n"
+                                    "RL_CALLOC"  "\n"
+                                    "RL_REALLOC" "\n"
+                                    "RL_FREE"    "\n"}})
+     (z/post-process-header-translation)
+     (z/zig :build (.getAbsolutePath ^java.io.File build-file) :run)
+     )
+    ;(.getAbsolutePath ^java.io.File build-file)
+
+    )
+
+  (z/translate-c-header!)
+  z/current-platform
+
+  (z/extract-zig-from-resources!)
+  (z/prepare-zig!)
+
+  (z/zig-command)
+
   (+ 1 2)
   ;NOTE: zigclj require runtime error: core.clj at (76:8): Keyword cannot be cast CharSequence
 
